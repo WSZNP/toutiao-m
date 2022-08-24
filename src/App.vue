b8cd829621c8879d6e579638072932fabbbc48d3
yhh <1903432957@qq.com>
2022-08-24 13:56:07 +0800

    项目初始化-引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,20 @@
     <!-- 路由出口 -->
     <router-view></router-view>
     <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-shipin"></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  // name: 'App',
 }
 </script>
 
